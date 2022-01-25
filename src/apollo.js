a7ec66078d9f3e3981b41cdea31d42237073ea6b
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-01-25 09:56:28 -0800

    Move apollo client config to its own file

---
+++ src/apollo.js
@@ -0,0 +1,109 @@
+import {
+  createHttpLink,
+  ApolloClient,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const subscriptionUri = "wss://cirqls-backend.herokuapp.com/subscriptions";
+
+const token = localStorage.getItem("token");
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach((error) => {
+      console.log(
+        `[GraphQL error]: Message: ${error.message}, Path: ${error.path}`
+      );
+
+      if (error.extensions.code === "UNAUTHENTICATED") {
+        console.log("TODO: Either redirect to /login or show a popup");
+      }
+    });
+
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+const wsLink = new WebSocketLink({
+  uri: subscriptionUri,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    },
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  errorLink.concat(authLink.concat(httpLink))
+);
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    User: {
+      keyFields: ["username"],
+      fields: {
+        followers: {
+          merge: false,
+        },
+      },
+    },
+    Community: {
+      keyFields: ["community_id"],
+      fields: {
+        members: {
+          merge: false,
+        },
+      },
+    },
+    Post: {
+      keyFields: ["post_id"],
+    },
+    Comment: {
+      keyFields: ["comment_id"],
+    },
+    Message: {
+      keyFields: ["message_id"],
+    },
+  },
+});
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: cache,
+  defaultOptions: {
+    query: {
+      errorPolicy: "all",
+    },
+  },
+});
