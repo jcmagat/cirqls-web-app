919c5a321723d6cfb124a0280f822772800acce4
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-02-10 16:31:23 -0800

    Add sort select field to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,18 +7,27 @@ import Paper from "@material-ui/core/Paper";
 import NavBar from "../components/Navigation/NavBar";
 import HomePageNav from "../components/Navigation/HomePageNav";
 import PostList from "../components/Post/PostList";
+import { MenuItem, TextField, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   postCards: {
     marginTop: 80,
+  },
+  sort: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    marginBottom: 16,
   },
 });
 
 function HomePage(props) {
   const classes = useStyles();
 
-  const [posts, setPosts] = useState([]);
   const [displayNav, setDisplayNav] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [sort, setSort] = useState("new");
 
   const { data } = useQuery(GET_HOME_PAGE_POSTS);
 
@@ -39,7 +48,25 @@ function HomePage(props) {
       <NavBar />
 
       <Paper className={classes.postCards} elevation={0}>
-        {displayNav ? <HomePageNav /> : <PostList posts={posts} />}
+        {displayNav ? (
+          <HomePageNav />
+        ) : (
+          <Paper elevation={0}>
+            <Paper className={classes.sort} elevation={0}>
+              <Typography>Sort :</Typography>
+              <TextField
+                select
+                value={sort}
+                onChange={(event) => setSort(event.target.value)}
+              >
+                <MenuItem value={"new"}>New</MenuItem>
+                <MenuItem value={"top"}>Top</MenuItem>
+              </TextField>
+            </Paper>
+
+            <PostList posts={posts} />
+          </Paper>
+        )}
       </Paper>
     </Container>
   );
