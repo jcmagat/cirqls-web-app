b2bc2b23e992c86316e8dad14165ac4477b1688f
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-02-23 11:37:31 -0800

    Move change username setting into its own component

---
+++ src/components/Settings/ChangeUsernameForm.jsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core";
+import { useAuthUser } from "../../context/AuthUserContext";
+import { useMutation } from "@apollo/client";
+import { CHANGE_USERNAME } from "../../graphql/mutations";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  buttonPaper: {
+    display: "flex",
+    alignItems: "center",
+    marginRight: 16,
+  },
+  buttons: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 8,
+  },
+  form: {
+    marginTop: 8,
+  },
+});
+
+function ChangeUsernameForm(props) {
+  const classes = useStyles();
+
+  const authUser = useAuthUser();
+
+  const [isChangeMode, setIsChangeMode] = useState(false);
+  const [newUsername, setNewUsername] = useState("");
+  const [newUsernameError, setNewUsernameError] = useState("");
+
+  // Resets the error message when the new username changes
+  useEffect(() => {
+    setNewUsernameError("");
+  }, [newUsername]);
+
+  useEffect(() => {
+    if (!authUser) return;
+
+    setNewUsername(authUser.username);
+  }, [authUser]);
+
+  const [changeUsername, { loading }] = useMutation(CHANGE_USERNAME, {
+    onCompleted: finishChangeUsername,
+    onError: handleError,
+  });
+
+  // Called when the save button is clicked
+  const handleChangeUsername = () => {
+    if (newUsername === authUser.username) {
+      setIsChangeMode(false);
+      return;
+    }
+
+    changeUsername({
+      variables: {
+        username: newUsername,
+      },
+    });
+  };
+
+  // Called when the change button is clicked
+  const handleChangeButtonClick = () => {
+    setIsChangeMode(true);
+  };
+
+  // Called when the cancel button is clicked
+  const handleCancel = () => {
+    setNewUsername(authUser.username);
+    setNewUsernameError("");
+    setIsChangeMode(false);
+  };
+
+  // Called after the mutation is completed
+  function finishChangeUsername() {
+    setIsChangeMode(false);
+  }
+
+  // Called when the mutation returns an error
+  function handleError(error) {
+    setNewUsernameError(error.message);
+  }
+
+  return (
+    <Paper className={classes.root} elevation={0}>
+      <Paper elevation={0}>
+        <Typography variant="body1">Username</Typography>
+        {isChangeMode ? (
+          <TextField
+            className={classes.form}
+            variant="outlined"
+            size="small"
+            id="username"
+            label="Username"
+            autoFocus
+            value={newUsername}
+            onChange={(event) => setNewUsername(event.target.value)}
+            disabled={loading}
+            error={Boolean(newUsernameError)}
+            helperText={newUsernameError}
+          />
+        ) : (
+          <Typography variant="body2">{`u/${authUser.username}`}</Typography>
+        )}
+      </Paper>
+
+      <Paper className={classes.buttonPaper} elevation={0}>
+        {isChangeMode ? (
+          <Paper className={classes.buttons} elevation={0}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={handleCancel}
+              disabled={loading}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={handleChangeUsername}
+              disabled={!newUsername || newUsernameError || loading}
+            >
+              Save
+            </Button>
+          </Paper>
+        ) : (
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleChangeButtonClick}
+          >
+            Change
+          </Button>
+        )}
+      </Paper>
+    </Paper>
+  );
+}
+
+export default ChangeUsernameForm;
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core";
 import { useAuthUser } from "../context/AuthUserContext";
-import { useMutation } from "@apollo/client";
-import { CHANGE_USERNAME } from "../graphql/mutations";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import NavBar from "../components/Navigation/NavBar";
 import ChangePasswordForm from "../components/Settings/ChangePasswordForm";
+import ChangeUsernameForm from "../components/Settings/ChangeUsernameForm";
 
 const useStyles = makeStyles({
   paper: {
@@ -30,61 +28,12 @@ const useStyles = makeStyles({
     alignItems: "center",
     marginRight: 16,
   },
-  twoButtons: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 8,
-  },
 });
 
 function SettingsPage(props) {
   const classes = useStyles();
 
   const authUser = useAuthUser();
-
-  /* ========== Change Username ========== */
-
-  const [isChangeUsernameMode, setIsChangeUsernameMode] = useState(false);
-  const [newUsername, setNewUsername] = useState("");
-
-  useEffect(() => {
-    if (!authUser) return;
-
-    setNewUsername(authUser.username);
-  }, [authUser]);
-
-  const [changeUsername, { loading: changeUsernameLoading }] = useMutation(
-    CHANGE_USERNAME,
-    {
-      onCompleted: finishChangeUsername,
-    }
-  );
-
-  const handleChangeUsername = () => {
-    if (newUsername === authUser.username) {
-      setIsChangeUsernameMode(false);
-      return;
-    }
-
-    changeUsername({
-      variables: {
-        username: newUsername,
-      },
-    });
-  };
-
-  const handleChangeUsernameButtonClick = () => {
-    setIsChangeUsernameMode(true);
-  };
-
-  const handleChangeUsernameCancel = () => {
-    setNewUsername(authUser.username);
-    setIsChangeUsernameMode(false);
-  };
-
-  function finishChangeUsername() {
-    setIsChangeUsernameMode(false);
-  }
 
   return (
     <Container>
@@ -110,56 +59,7 @@ function SettingsPage(props) {
               </Paper>
             </Paper>
 
-            <Paper className={classes.setting} elevation={0}>
-              <Paper elevation={0}>
-                <Typography variant="body1">Username</Typography>
-                {isChangeUsernameMode ? (
-                  <TextField
-                    variant="outlined"
-                    size="small"
-                    id="username"
-                    label="Username"
-                    autoFocus
-                    value={newUsername}
-                    onChange={(event) => setNewUsername(event.target.value)}
-                    disabled={changeUsernameLoading}
-                  />
-                ) : (
-                  <Typography variant="body2">{`u/${authUser.username}`}</Typography>
-                )}
-              </Paper>
-
-              <Paper className={classes.buttonPaper} elevation={0}>
-                {isChangeUsernameMode ? (
-                  <Paper className={classes.twoButtons} elevation={0}>
-                    <Button
-                      variant="outlined"
-                      color="secondary"
-                      onClick={handleChangeUsernameCancel}
-                      disabled={changeUsernameLoading}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      variant="outlined"
-                      color="primary"
-                      onClick={handleChangeUsername}
-                      disabled={changeUsernameLoading}
-                    >
-                      Save
-                    </Button>
-                  </Paper>
-                ) : (
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    onClick={handleChangeUsernameButtonClick}
-                  >
-                    Change
-                  </Button>
-                )}
-              </Paper>
-            </Paper>
+            <ChangeUsernameForm />
 
             <ChangePasswordForm />
           </Paper>
