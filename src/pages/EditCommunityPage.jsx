f26691d2986242c70d09a7d2d298fcd18c8a51eb
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-03-08 12:47:55 -0800

    Improve edit community page

--- src/pages/EditCommunityPage.jsx
+++ src/pages/EditCommunityPage.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/styles";
 import { useCommunity } from "../context/CommunityContext";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import { Avatar, Button, Typography } from "@material-ui/core";
+import Badge from "@material-ui/core/Badge";
+import IconButton from "@material-ui/core/IconButton";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 import NavBar from "../components/Navigation/NavBar";
+import UploadDialog from "../components/Common/UploadDialog";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   paper: {
     marginTop: 80,
+    display: "flex",
+    flexDirection: "column",
+    gap: 16,
+  },
+  paperCentered: {
+    display: "flex",
+    justifyContent: "center",
   },
   avatar: {
     width: 80,
@@ -22,6 +36,17 @@ const useStyles = makeStyles({
     gap: 16,
     width: "max-content",
   },
+  buttons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 8,
+  },
+  delete: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 32,
+  },
 });
 
 function EditCommunityPage(props) {
@@ -29,10 +54,29 @@ function EditCommunityPage(props) {
 
   const community = useCommunity();
 
-  const [logoSrc, setLogoSrc] = useState("");
-  const [name, setName] = useState("");
+  const [uploadDialogOpen, setUploadDialogOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [newLogo, setNewLogo] = useState("");
+
+  useEffect(() => {
+    if (!community) return;
+
+    setTitle(community.title);
+    setDescription(community.description);
+  }, [community]);
+
+  const handleEditLogo = () => {
+    setUploadDialogOpen(true);
+  };
+
+  const handleCloseUploadDialog = () => {
+    setUploadDialogOpen(false);
+  };
+
+  const handleChangeLogo = (logo) => {
+    setNewLogo(logo);
+  };
 
   return (
     <Container>
@@ -40,15 +84,68 @@ function EditCommunityPage(props) {
 
       {community && (
         <Paper className={classes.paper} elevation={0}>
-          <Avatar className={classes.avatar}>
-            {community.name.charAt(0).toUpperCase()}
-          </Avatar>
+          <Paper className={classes.paperCentered} elevation={0}>
+            <Badge
+              overlap="circular"
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              badgeContent={
+                <IconButton
+                  onClick={handleEditLogo}
+                  // disabled={changeProfilePicLoading}
+                >
+                  <EditOutlinedIcon />
+                </IconButton>
+              }
+            >
+              <Avatar
+                className={classes.avatar}
+                src={
+                  newLogo ? URL.createObjectURL(newLogo) : community.logo_src
+                }
+              />
+            </Badge>
+          </Paper>
 
-          <Typography>{community.name}</Typography>
+          <UploadDialog
+            open={uploadDialogOpen}
+            onClose={handleCloseUploadDialog}
+            onChange={handleChangeLogo}
+          />
 
-          <Typography>{community.title}</Typography>
+          <Paper className={classes.paperCentered} elevation={0}>
+            <Typography variant="h5">{`c/${community.name}`}</Typography>
+          </Paper>
 
-          <Typography>{community.description}</Typography>
+          <TextField
+            className={classes.form}
+            variant="outlined"
+            size="small"
+            id="title"
+            label="Title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            // disabled={loading}
+            // error={Boolean(newUsernameError)}
+            // helperText={newUsernameError}
+          />
+
+          <TextField
+            className={classes.form}
+            variant="outlined"
+            size="small"
+            id="description"
+            label="Description"
+            multiline
+            rows={8}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            // disabled={loading}
+            // error={Boolean(newUsernameError)}
+            // helperText={newUsernameError}
+          />
 
           <Typography>Moderators:</Typography>
 
@@ -56,6 +153,7 @@ function EditCommunityPage(props) {
             <Paper
               className={classes.moderator}
               elevation={0}
+              key={moderator.user_id}
               component={Link}
               to={`/u/${moderator.username}`}
             >
@@ -64,13 +162,38 @@ function EditCommunityPage(props) {
             </Paper>
           ))}
 
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ width: "max-content" }}
+          >
             + Invite
           </Button>
 
-          <Button variant="outlined" color="secondary">
-            Delete Circle
-          </Button>
+          <Paper className={classes.buttons} elevation={0}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              component={Link}
+              to={`/c/${community.name}`}
+            >
+              Cancel
+            </Button>
+
+            <Button variant="contained" color="primary">
+              Save
+            </Button>
+          </Paper>
+
+          <Paper className={classes.delete} elevation={0}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              style={{ width: "max-content" }}
+            >
+              Delete Circle
+            </Button>
+          </Paper>
         </Paper>
       )}
     </Container>
