df48bcb0f63323147afd917be7e01e2270b20d0e
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-05-13 12:03:32 -0700

    Hide comments in post page depending on screen size

--- src/components/Comment/CommentTree.jsx
+++ src/components/Comment/CommentTree.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { styled } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import TreeView from "@mui/lab/TreeView";
 import TreeItem from "@mui/lab/TreeItem";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import CommentCard from "./CommentCard";
@@ -23,7 +25,15 @@ const StyledTreeItem = styled(TreeItem)(({ theme }) => ({
 }));
 
 function CommentTree({ comments, comment_ids, ref_comment_id }) {
-  // TODO: have maxHeight based on screen size
+  let maxHeight = 20;
+
+  if (useMediaQuery((theme) => theme.breakpoints.down("md"))) {
+    maxHeight = 10;
+  }
+
+  if (useMediaQuery((theme) => theme.breakpoints.down("sm"))) {
+    maxHeight = 4;
+  }
 
   const [expanded, setExpanded] = useState([]);
 
@@ -35,32 +45,51 @@ function CommentTree({ comments, comment_ids, ref_comment_id }) {
     if (node) node.scrollIntoView({ behavior: "smooth", block: "center" });
   }, []);
 
-  const renderCommentTree = (comment) => {
-    return (
-      <StyledTreeItem
-        key={comment.comment_id}
-        nodeId={comment.comment_id.toString()}
-        ref={comment.comment_id === ref_comment_id ? commentRef : null}
-        label={
-          !expanded.includes(comment.comment_id.toString()) && (
-            <Typography variant="body2">
-              {`Expand u/${comment.commenter.username}'s comment`}
-            </Typography>
-          )
-        }
-      >
-        <CommentCard
-          comment={comment}
-          elevation={comment.comment_id === ref_comment_id ? 16 : 1}
-        />
+  const renderCommentTree = (comment, height) => {
+    if (!comment) return;
 
-        {Array.isArray(comment.child_comments)
-          ? comment.child_comments.map((comments) =>
-              renderCommentTree(comments)
+    if (height <= maxHeight) {
+      return (
+        <StyledTreeItem
+          key={comment.comment_id}
+          nodeId={comment.comment_id.toString()}
+          ref={comment.comment_id === ref_comment_id ? commentRef : null}
+          label={
+            !expanded.includes(comment.comment_id.toString()) && (
+              <Typography variant="body2">
+                {`Expand u/${comment.commenter.username}'s comment`}
+              </Typography>
             )
-          : null}
-      </StyledTreeItem>
-    );
+          }
+        >
+          <CommentCard
+            comment={comment}
+            elevation={comment.comment_id === ref_comment_id ? 16 : 1}
+          />
+
+          {
+            // Only render 1 child comment past maxHeight (which will be a
+            // button to show more comments, see else condition below)
+            height < maxHeight
+              ? Array.isArray(comment.child_comments) &&
+                comment.child_comments.map((comments) =>
+                  renderCommentTree(comments, height + 1)
+                )
+              : Array.isArray(comment.child_comments) &&
+                renderCommentTree(comment.child_comments[0], height + 1)
+          }
+        </StyledTreeItem>
+      );
+    } else {
+      return (
+        <StyledTreeItem
+          key={comment.comment_id}
+          nodeId={comment.comment_id.toString()}
+        >
+          <Button variant="outlined">More Comments</Button>
+        </StyledTreeItem>
+      );
+    }
   };
 
   return (
@@ -70,7 +99,7 @@ function CommentTree({ comments, comment_ids, ref_comment_id }) {
       defaultExpandIcon={<ChevronRightIcon />}
       onNodeToggle={(event, nodeIds) => setExpanded(nodeIds)}
     >
-      {comments.map((comment) => renderCommentTree(comment))}
+      {comments.map((comment) => renderCommentTree(comment, 1))}
     </TreeView>
   );
 }
