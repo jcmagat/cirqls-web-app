9af218624e09a043ad6ed438d1ad9cb4e5b8c5fb
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-05-16 08:46:45 -0700

    Improve change password form in settings

--- src/components/Settings/ChangePasswordForm.tsx
+++ src/components/Settings/ChangePasswordForm.tsx
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from "react";
-import makeStyles from "@mui/styles/makeStyles";
-import { useMutation } from "@apollo/client";
+import { useState, useEffect } from "react";
+import { useMutation, ApolloError } from "@apollo/client";
 import { CHANGE_PASSWORD } from "../../graphql/mutations";
 import isStrongPassword from "validator/lib/isStrongPassword";
-import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import StyledButtonBox from "./StyledButtonBox";
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  buttonPaper: {
-    display: "flex",
-    alignItems: "center",
-    marginRight: 16,
-  },
-  buttons: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 8,
-  },
-  form: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 32,
-  },
-  textfields: {
-    display: "flex",
-    flexDirection: "column",
-    gap: 8,
-    marginTop: 8,
-  },
-});
-
-function ChangePasswordForm(props) {
-  const classes = useStyles();
-
+function ChangePasswordForm() {
   const [isChangeMode, setIsChangeMode] = useState(false);
 
   const [currentPassword, setCurrentPassword] = useState("");
@@ -78,6 +48,20 @@ function ChangePasswordForm(props) {
 
   // Called when the save button is clicked
   const handleChangePassword = () => {
+    if (currentPasswordError || newPasswordError || confirmNewPasswordError)
+      return;
+
+    if (!currentPassword) {
+      setCurrentPasswordError("Please confirm your current password");
+      return;
+    } else if (!newPassword) {
+      setNewPasswordError("Please provide a new password");
+      return;
+    } else if (!confirmNewPassword) {
+      setConfirmNewPasswordError("Please confirm your new password");
+      return;
+    }
+
     changePassword({
       variables: {
         current_password: currentPassword,
@@ -114,71 +98,90 @@ function ChangePasswordForm(props) {
   }
 
   // Called when the mutation returns an error
-  function handleError(error) {
+  function handleError(error: ApolloError) {
     setCurrentPasswordError(error.message);
   }
 
   return (
-    <Paper className={classes.root} elevation={0}>
-      <Paper elevation={0}>
-        <Typography variant="body1">Password</Typography>
-        {isChangeMode ? (
-          <Paper className={classes.form} elevation={0}>
-            <Paper className={classes.textfields} elevation={0}>
+    <Box
+      sx={{
+        position: "relative",
+        marginRight: 2,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="body1">Password</Typography>
+
+      {isChangeMode ? (
+        <>
+          <Box
+            sx={{
+              marginTop: 1,
+              display: "flex",
+              flexDirection: "row",
+              gap: 2,
+            }}
+          >
+            <Box
+              sx={{
+                width: 300,
+                display: "flex",
+                flexDirection: "column",
+                gap: 1,
+              }}
+            >
               <TextField
                 size="small"
                 type="password"
                 id="current-password"
                 label="Current Password"
-                required
                 autoFocus
                 onChange={(event) => setCurrentPassword(event.target.value)}
                 disabled={loading}
                 error={Boolean(currentPasswordError)}
                 helperText={currentPasswordError}
               />
+
               <TextField
                 size="small"
                 type="password"
                 id="new-password"
                 label="New Password"
-                required
                 onChange={(event) => setNewPassord(event.target.value)}
                 disabled={loading}
                 error={Boolean(newPasswordError)}
                 helperText={newPasswordError}
               />
+
               <TextField
                 size="small"
                 type="password"
                 id="confirm-new-password"
                 label="Confirm New Password"
-                required
                 onChange={(event) => setConfirmNewPassord(event.target.value)}
                 disabled={loading}
                 error={Boolean(confirmNewPasswordError)}
                 helperText={confirmNewPasswordError}
               />
-            </Paper>
+            </Box>
 
-            <Paper elevation={0}>
-              <Typography variant="body2">
-                Password must contain:
-                <li>Minimum 8 characters</li>
-                <li>At least 1 uppercase</li>
-                <li>At least 1 lowercase</li>
-                <li>At least 1 number</li>
-              </Typography>
-            </Paper>
-          </Paper>
-        ) : (
-          <Typography variant="body2">********</Typography>
-        )}
-      </Paper>
+            <Typography variant="body2">
+              Password must contain at least:
+              <li>8 characters</li>
+              <li>1 lower case letter</li>
+              <li>1 upper case letter</li>
+              <li>1 number</li>
+            </Typography>
+          </Box>
 
-      <Paper className={classes.buttonPaper} elevation={0}>
-        {isChangeMode ? (
-          <Paper className={classes.buttons} elevation={0}>
+          <StyledButtonBox
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 1,
+            }}
+          >
             <Button
               variant="outlined"
               color="secondary"
@@ -187,34 +190,33 @@ function ChangePasswordForm(props) {
             >
               Cancel
             </Button>
+
             <Button
               variant="outlined"
               color="primary"
               onClick={handleChangePassword}
-              disabled={
-                !currentPassword ||
-                Boolean(currentPasswordError) ||
-                !newPassword ||
-                Boolean(newPasswordError) ||
-                !confirmNewPassword ||
-                Boolean(confirmNewPasswordError) ||
-                loading
-              }
+              disabled={loading}
             >
               Save
             </Button>
-          </Paper>
-        ) : (
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={handleChangeButtonClick}
-          >
-            Change
-          </Button>
-        )}
-      </Paper>
-    </Paper>
+          </StyledButtonBox>
+        </>
+      ) : (
+        <>
+          <Typography variant="body2">********</Typography>
+
+          <StyledButtonBox>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={handleChangeButtonClick}
+            >
+              Change
+            </Button>
+          </StyledButtonBox>
+        </>
+      )}
+    </Box>
   );
 }
 
--- src/components/Settings/StyledButtonBox.tsx
+++ src/components/Settings/StyledButtonBox.tsx
@@ -2,12 +2,12 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 
 const StyledButtonBox = styled(Box)(({ theme }) => ({
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up("md")]: {
     position: "absolute",
     top: 0,
     right: 0,
   },
-  [theme.breakpoints.down("sm")]: {
+  [theme.breakpoints.down("md")]: {
     position: "relative",
     marginTop: 8,
   },
