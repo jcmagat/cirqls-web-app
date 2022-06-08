f51e4e403694754a66d2aa66a0ebbba55e3881ac
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-06-08 11:29:36 -0700

    Reimplement Google OAuth using the new API

--- src/components/Common/GoogleOAuthButton.tsx
+++ src/components/Common/GoogleOAuthButton.tsx
@@ -1,7 +1,4 @@
-import { SxProps } from "@mui/material";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import GoogleButton from "react-google-button";
+import { Button, SxProps } from "@mui/material";
 
 interface Props {
   label?: string;
@@ -11,35 +8,16 @@ interface Props {
 function GoogleOAuthButton(props: Props) {
   const { label, sx } = props;
 
-  const getGoogleOAuthURL = () => {
-    const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-
-    const options = {
-      redirect_uri: process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL as string,
-      client_id: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string,
-      access_type: "offline",
-      response_type: "code",
-      prompt: "consent",
-      scope: [
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/userinfo.profile",
-      ].join(" "),
-    };
-
-    const queryString = new URLSearchParams(options);
-
-    return `${rootUrl}?${queryString.toString()}`;
-  };
+  // Request an access token
+  function getAuthCode() {
+    // Request authorization code and obtain user consent
+    (window as any).client.requestCode();
+  }
 
   return (
-    <Box sx={{ ...sx }}>
-      <Link href={getGoogleOAuthURL()}>
-        <GoogleButton
-          type="light"
-          label={label ? label : " Continue with Google"}
-        />
-      </Link>
-    </Box>
+    <Button variant="outlined" onClick={getAuthCode}>
+      Sign In with Google
+    </Button>
   );
 }
 
